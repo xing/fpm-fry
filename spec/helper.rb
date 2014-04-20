b4411625324539bd0e27d6c17ba49c2ae9c84c36
Hannes Georg <hannes.georg@xing.com>
2014-04-20 15:56:36 +0200

    remove os_db from coverage

--- spec/helper.rb
+++ spec/helper.rb
@@ -6,6 +6,7 @@ SimpleCov.start do
     Coveralls::SimpleCov::Formatter
   ]
   add_filter "/spec"
+  add_filter "lib/fpm/dockery/os_db.rb"
   maximum_coverage_drop 5
 end
 RSpec.configure do |config|
