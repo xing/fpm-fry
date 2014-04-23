898d9566b727095b31ff731fc364c3fbe4ac98a9
Hannes Georg <hannes.georg@xing.com>
2014-04-23 14:15:14 +0200

    added specs for stream parser instances

--- spec/stream_parser_spec.rb
+++ spec/stream_parser_spec.rb
@@ -84,6 +84,58 @@ describe FPM::Dockery::StreamParser do
 
   end
 
+  describe 'Instance' do
 
+    let(:stack){
+      s = double(:stack)
+      allow(s).to receive(:response_call){|arg| arg }
+      s
+    }
+    let(:socket){
+      double(:socket)
+    }
+    let(:connection){
+      con = double(:connetion)
+      allow(con).to receive(:socket){ socket }
+      con
+    }
+    let(:datum){
+      { connection: connection }
+    }
+
+    subject{ described_class.new( out, err ).new(stack) }
+
+    context 'empty response' do
+      let(:socket){
+        StringIO.new(<<IO)
+HTTP/1.1 200 Success
+
+IO
+      }
+      it "works" do
+        # no assertions here because the output stream have implict assertions
+        subject.response_call(datum)
+      end
+    end
+
+    context 'simple response' do
+      let(:socket){
+        StringIO.new(<<IO.chomp)
+HTTP/1.1 200 Success
+Foo: Bar
+
+#{[2,6,"stderr",1,6,"stdout"].pack("I<I>Z6I<I>Z6")}
+IO
+      }
+      it "works" do
+        expect(err).to receive(:write).with("stderr")
+        expect(out).to receive(:write).with("stdout")
+        res = subject.response_call(datum)
+        expect( res[:response][:headers] ).to eq('Foo' => 'Bar')
+      end
+    end
+
+
+  end
 
 end
